3056d21ee05c9c8378f42440d9cb6a1f3e6bb2b2
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2025-11-10 14:22:09 +0200

    add complete LDAC.bib

---
+++ ldac/README.md
@@ -0,0 +1,21 @@
+# LDAC Bibliography
+
+Linked Data in Architecture and Construction (LDAC) is the premier conference on Semantic BIM.
+It's actually a workshop and a summer school, and manages to attract an excellent and very supportive community.
+If you haven't attended yet, I highly recommend it!
+
+[LDAC.bib](LDAC.bib) has full bibliographies of LDAC 2018-2025; 2017 and earlier years have group reports not individual papers.
+
+The [Semantic BIM](https://www.zotero.org/groups/3007408/semantic_bim/library) Zotero library also has full bibliographies,
+see the [0 Proceedings](https://www.zotero.org/groups/3007408/semantic_bim/collections/XGS5UCIF) collection.
+- Recent LDAC workshops (but not 2025) have been published at CEUR-WS.
+  This makes it easy to obtain a bibliography, since Zotero now has an improved CEUR-WS plugin (parser).
+  For the rest, I've made `.bib` files using the Emacs editor, or perl scripts (e.g. [ldac2025.pl](ldac2025.pl))
+- LDAC is often collocated with the CIM W78, EG-ICE and/or EC3 conferences.
+  This makes it a bit harder to extract paper links.
+  [LDAC2021-get.sh](LDAC2021-get.sh) is a script to download all LDAC 2021 papers and presentations from the LDAC 2021 and W80 2021 sites.
+
+We are looking for your help to dispatch these excellent contributions into the Zotero topical hierarchy! 
+- Please join the [Semantic BIM](https://www.zotero.org/groups/3007408/semantic_bim/) group (as of Nov 2025, it has 47 members)
+- Then "copy" items from `0 Proceedings` into the topical sub-collections, add tags, and Abstracts
+- Feel free to create new topical branches as needed
---
+++ ldac/ldac2025.pl
@@ -0,0 +1,73 @@
+﻿#!perl -wp0
+s{ *<li style="margin-left: 25px;">
+   *    <b>(.*)</b>
+   *    <br/>(.*)
+   *    <br/><a href="(files/papers/(LDAC2025_paper_)?(.*).pdf)">article</a>
+   *    <br/><a href="(.*)">presentation</a>
+   *</li>}
+  {\@InProceedings{LDAC_2025_$5,
+  title        = {$1},
+  author       = {$2},
+  booktitle    = {Linked Data in Architecture and Construction (LDAC 2025)},
+  year         = 2025,
+  month        = jul,
+  url          = {https://linkedbuildingdata.net/ldac2025/$3},
+  date         = {2025-07},
+  address      = {Porto, Portugal},
+  annotation   = {Presentation: https://linkedbuildingdata.net/ldac2025/$6}
+}
+}g;
+
+s{ *<li style="margin-left: 25px;">
+   *    <b>(.*)</b>
+   *    <br/>(.*)
+   *    <br/><a href="(files/papers/(LDAC2025_paper_)?(.*).pdf)">article</a>
+   *</li>}
+  {\@InProceedings{LDAC_2025_$5,
+  title        = {$1},
+  author       = {$2},
+  booktitle    = {Linked Data in Architecture and Construction (LDAC 2025)},
+  year         = 2025,
+  month        = jul,
+  url          = {https://linkedbuildingdata.net/ldac2025/$3},
+  date         = {2025-07},
+  address      = {Porto, Portugal}
+}
+}g;
+
+s{ *(<!--)?<li style="margin-left: 25px;">
+   *    <b>(.*)</b>
+   *    <br/>(.*)
+   *    <br/><a href="(files/posters/LDAC2025_(Poster_.*)_final.pdf)">extended abstract</a>
+   *</li>(-->)?}
+  {\@InProceedings{LDAC_2025_$5,
+  title        = {$2},
+  author       = {$3},
+  booktitle    = {Linked Data in Architecture and Construction (LDAC 2025)},
+  year         = 2025,
+  month        = jul,
+  url          = {https://linkedbuildingdata.net/ldac2025/$4},
+  date         = {2025-07},
+  address      = {Porto, Portugal},
+  annotation   = {type: extended abstract}
+}
+}g;
+
+s{ *<li style="margin-left: 25px;">
+   *    <b>(.*)</b>
+   *    <br/>(.*)
+   *</li>}
+  {\@InProceedings{LDAC_2025_Poster_xx,
+  title        = {$1},
+  author       = {$2},
+  booktitle    = {Linked Data in Architecture and Construction (LDAC 2025)},
+  year         = 2025,
+  month        = jul,
+  date         = {2025-07},
+  address      = {Porto, Portugal},
+  annotation   = {type: extended abstract}
+}
+}g;
+
+# in "author": replace ", " with " and "
+do {} while s{^(  author[^\}]+?), }{$1 and }m;
